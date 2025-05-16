84d031294c48cb587d51e586cd98b0fcbe499abe
Pramod Chavhan <147310528+Pramod-Chavhan@users.noreply.github.com>
2025-05-16 12:53:20 +0530

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -119,7 +119,7 @@ export default function HeroSection() {
               } flex items-center justify-center transition-colors duration-300`}
             >
               <img
-                src="/"
+                src="public/pic.jpg"
                 alt="Transparent Image"
                 className="h-full object-contain z-10 relative"
               />
